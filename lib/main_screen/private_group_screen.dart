98f5cdfe36389d8f727f544205bace1de8f78619
Dexterfury <raphaelsquared@gmail.com>
2024-08-13 19:48:18 +0200

    added private group search

--- lib/main_screen/private_group_screen.dart
+++ lib/main_screen/private_group_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_chat_pro/constants.dart';
@@ -15,6 +17,7 @@ class PrivateGroupScreen extends StatefulWidget {
 }
 
 class _PrivateGroupScreenState extends State<PrivateGroupScreen> {
+  String searchQuery = '';
   @override
   Widget build(BuildContext context) {
     final uid = context.read<AuthenticationProvider>().userModel!.uid;
@@ -24,61 +27,158 @@ class _PrivateGroupScreenState extends State<PrivateGroupScreen> {
         Padding(
           padding: const EdgeInsets.all(8.0),
           child: CupertinoSearchTextField(
-            onChanged: (value) {},
+            placeholder: 'Search',
+            onChanged: (value) => setState(() => searchQuery = value),
+            onSuffixTap: () {
+              setState(() => searchQuery = '');
+              FocusScope.of(context).unfocus();
+            },
           ),
         ),
-
-        // stream builder for private groups
-        StreamBuilder<List<GroupModel>>(
-          stream:
-              context.read<GroupProvider>().getPrivateGroupsStream(userId: uid),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
-            }
-            if (snapshot.hasError) {
-              return const Center(
-                child: Text('Something went wrong'),
-              );
-            }
-            if (snapshot.data!.isEmpty) {
-              return const Center(
-                child: Text('No private groups'),
-              );
-            }
-            return Expanded(
-              child: ListView.builder(
-                itemCount: snapshot.data!.length,
-                itemBuilder: (context, index) {
-                  final groupModel = snapshot.data![index];
-                  return ChatWidget(
-                      group: groupModel,
-                      isGroup: true,
-                      onTap: () {
-                        context
-                            .read<GroupProvider>()
-                            .setGroupModel(groupModel: groupModel)
-                            .whenComplete(() {
-                          Navigator.pushNamed(
-                            context,
-                            Constants.chatScreen,
-                            arguments: {
-                              Constants.contactUID: groupModel.groupId,
-                              Constants.contactName: groupModel.groupName,
-                              Constants.contactImage: groupModel.groupImage,
-                              Constants.groupId: groupModel.groupId,
-                            },
-                          );
-                        });
-                      });
-                },
-              ),
-            );
-          },
-        )
+        if (searchQuery == '')
+          MyPrivateGroups(uid: uid)
+        else
+          MyPrivateSearchGroups(uid: uid, searchText: searchQuery)
       ],
     ));
+  }
+}
+
+class MyPrivateGroups extends StatelessWidget {
+  const MyPrivateGroups({
+    super.key,
+    required this.uid,
+  });
+
+  final String uid;
+
+  @override
+  Widget build(BuildContext context) {
+    log('this one');
+    return StreamBuilder<List<GroupModel>>(
+      stream: context.read<GroupProvider>().getPrivateGroupsStream(userId: uid),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+        if (snapshot.hasError) {
+          return const Center(
+            child: Text('Something went wrong'),
+          );
+        }
+        if (snapshot.data!.isEmpty) {
+          return const Center(
+            child: Text('No private groups'),
+          );
+        }
+        return Expanded(
+          child: ListView.builder(
+            itemCount: snapshot.data!.length,
+            itemBuilder: (context, index) {
+              final groupModel = snapshot.data![index];
+              return ChatWidget(
+                  group: groupModel,
+                  isGroup: true,
+                  onTap: () {
+                    context
+                        .read<GroupProvider>()
+                        .setGroupModel(groupModel: groupModel)
+                        .whenComplete(() {
+                      Navigator.pushNamed(
+                        context,
+                        Constants.chatScreen,
+                        arguments: {
+                          Constants.contactUID: groupModel.groupId,
+                          Constants.contactName: groupModel.groupName,
+                          Constants.contactImage: groupModel.groupImage,
+                          Constants.groupId: groupModel.groupId,
+                        },
+                      );
+                    });
+                  });
+            },
+          ),
+        );
+      },
+    );
+  }
+}
+
+class MyPrivateSearchGroups extends StatelessWidget {
+  const MyPrivateSearchGroups({
+    super.key,
+    required this.uid,
+    required this.searchText,
+  });
+
+  final String uid;
+  final String searchText;
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<List<GroupModel>>(
+      stream: context.read<GroupProvider>().getPrivateGroupsStream(userId: uid),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+        if (snapshot.hasError) {
+          return const Center(
+            child: Text('Something went wrong'),
+          );
+        }
+
+        final results = snapshot.data!
+            .where((element) => element.groupName
+                .toString()
+                .toLowerCase()
+                .contains(searchText.toLowerCase()))
+            .toList();
+
+        if (results.isEmpty) {
+          return const Center(
+            child: Text('No group found'),
+          );
+        }
+
+        if (results.isNotEmpty) {
+          return Expanded(
+            child: ListView.builder(
+              itemCount: snapshot.data!.length,
+              itemBuilder: (context, index) {
+                final groupModel = snapshot.data![index];
+                return ChatWidget(
+                    group: groupModel,
+                    isGroup: true,
+                    onTap: () {
+                      context
+                          .read<GroupProvider>()
+                          .setGroupModel(groupModel: groupModel)
+                          .whenComplete(() {
+                        Navigator.pushNamed(
+                          context,
+                          Constants.chatScreen,
+                          arguments: {
+                            Constants.contactUID: groupModel.groupId,
+                            Constants.contactName: groupModel.groupName,
+                            Constants.contactImage: groupModel.groupImage,
+                            Constants.groupId: groupModel.groupId,
+                          },
+                        );
+                      });
+                    });
+              },
+            ),
+          );
+        }
+        return const Center(
+          child: Text('No group found'),
+        );
+      },
+    );
   }
 }
