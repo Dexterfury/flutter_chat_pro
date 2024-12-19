f88b6e74ea603b312e27650d37b0faa896dadcd1
Dexterfury <raphaelsquared@gmail.com>
2024-12-19 16:21:00 +0200

    refactor: streamline group creation UI and enhance search functionality

--- lib/main_screen/create_group_screen.dart
+++ lib/main_screen/create_group_screen.dart
@@ -215,72 +215,70 @@ class _CreateGroupScreenState extends State<CreateGroupScreen> {
                 ],
               ),
 
-            // texField for group name
-            TextField(
-              controller: groupNameController,
-              maxLength: 25,
-              textInputAction: TextInputAction.next,
-              decoration: const InputDecoration(
-                hintText: 'Group Name',
-                label: Text('Group Name'),
-                counterText: '',
-                border: OutlineInputBorder(),
-              ),
-            ),
-            const SizedBox(height: 10),
-            // textField for group description
-            TextField(
-              controller: groupDescriptionController,
-              maxLength: 100,
-              textInputAction: TextInputAction.done,
-              decoration: const InputDecoration(
-                hintText: 'Group Description',
-                label: Text('Group Description'),
-                counterText: '',
-                border: OutlineInputBorder(),
-              ),
-            ),
-            const SizedBox(height: 10),
-            Card(
-              child: Padding(
-                padding: const EdgeInsets.only(
-                  left: 8.0,
-                  right: 8.0,
+              // texField for group name
+              TextField(
+                controller: groupNameController,
+                maxLength: 25,
+                textInputAction: TextInputAction.next,
+                decoration: const InputDecoration(
+                  hintText: 'Group Name',
+                  label: Text('Group Name'),
+                  counterText: '',
+                  border: OutlineInputBorder(),
                 ),
-                child: SettingsListTile(
-                    title: 'Group Settings',
-                    icon: Icons.settings,
-                    iconContainerColor: Colors.deepPurple,
-                    onTap: () {
-                      // navigate to group settings screen
-                      Navigator.pushNamed(
-                          context, Constants.groupSettingsScreen);
-                    }),
               ),
-            ),
-            const SizedBox(height: 10),
-            const Text(
-              'Select Group Members',
-              style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
+              // textField for group description
+              TextField(
+                controller: groupDescriptionController,
+                maxLength: 100,
+                textInputAction: TextInputAction.done,
+                decoration: const InputDecoration(
+                  hintText: 'Group Description',
+                  label: Text('Group Description'),
+                  counterText: '',
+                  border: OutlineInputBorder(),
+                ),
               ),
-            ),
-            const SizedBox(height: 10),
-
-            // cuppertino search bar
-            CupertinoSearchTextField(
-              onChanged: (value) {},
-            ),
-
-            const SizedBox(height: 10),
-
-            const Expanded(
-              child: FriendsList(
-                viewType: FriendViewType.groupView,
+              Card(
+                child: Padding(
+                  padding: const EdgeInsets.only(
+                    left: 8.0,
+                    right: 8.0,
+                  ),
+                  child: SettingsListTile(
+                      title: 'Group Settings',
+                      icon: Icons.settings,
+                      iconContainerColor: Colors.deepPurple,
+                      onTap: () {
+                        // navigate to group settings screen
+                        Navigator.pushNamed(
+                            context, Constants.groupSettingsScreen);
+                      }),
+                ),
               ),
-            ),
-          ],
+              const Text(
+                'Select Group Members',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+
+              // Search bar
+              SearchBarWidget(
+                onChanged: (value) {
+                  context.read<SearchProvider>().setSearchQuery(value);
+                },
+              ),
+
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.3,
+                child: const FriendsList(
+                  viewType: FriendViewType.groupView,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/main_screen/group_settings_screen.dart
+++ lib/main_screen/group_settings_screen.dart
@@ -1,6 +1,4 @@
 import 'dart:io';
-
-import 'package:date_format/date_format.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_chat_pro/enums/enums.dart';
 import 'package:flutter_chat_pro/models/user_model.dart';
