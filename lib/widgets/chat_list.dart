727937ef2afe371707126e3de05f1b7734abbb22
Dexterfury <raphaelsquared@gmail.com>
2025-01-02 11:33:25 +0200

    Add comment for showDeletBottomSheet method in ChatList widget

--- lib/widgets/chat_list.dart
+++ lib/widgets/chat_list.dart
@@ -96,6 +96,7 @@ class _ChatListState extends State<ChatList> {
     }
   }
 
+  // Delet bottom sheet
   void showDeletBottomSheet({
     required MessageModel message,
     required String currentUserId,
